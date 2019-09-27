c967fde241fd551eb40b16db9705f807bd90e0ca
Mikkel Bo Christensen <mc@airsupport.dk>
2019-09-28 00:17:40 +0200

    some layout and folder structure changes

---
+++ src/app/containers/layout-grid/layout-grid.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-grid',
+  templateUrl: './layout-grid.component.html',
+  styleUrls: ['./layout-grid.component.css']
+})
+export class LayoutGridComponent { }
